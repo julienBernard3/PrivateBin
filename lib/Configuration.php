6db3b0e470f5c64e46ddfcabaedb42abd1ebf16c
julienBernard3 <101281916+julienBernard3@users.noreply.github.com>
2023-10-12 11:59:45 +0200

    Update Configuration.php correction erreur créer plus tot

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -278,8 +278,7 @@ class Configuration
     {
         $options = $this->getSection($section);
         if (!array_key_exists($key, $options)) {
-            $i18n = new \PrivateBin\I18n();
-            throw new Exception($I18n::_('Invalid data.') . " $section / $key", 4);
+            throw new Exception(I18n::_('Invalid data.') . " $section / $key", 4);
         }
         return $this->_configuration[$section][$key];
     }
